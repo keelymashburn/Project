efe562756b9979aaa2b18138d28a6422aa7ed51e
Keely Mashburn <39414616+keelymashburn@users.noreply.github.com>
2022-01-02 23:52:22 -0500

    end of section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -23,8 +23,7 @@ export class AccountService {
         const user = response;
         if(user)
         {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -36,8 +35,7 @@ export class AccountService {
       map((user: User) => {
         if(user)
         {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -45,6 +43,7 @@ export class AccountService {
 
   setCurrentUser(user: User)
   {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
