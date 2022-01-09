0dd91d64d040a1770e91eee12b12f5d8862fd41a
Keely Mashburn <39414616+keelymashburn@users.noreply.github.com>
2022-01-09 17:01:49 -0500

    end of section 18

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/_models/message';
+import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css']
@@ -13,17 +15,15 @@ export class MemberMessagesComponent implements OnInit {
   @Input() messages: Message[];
   @Input() username: string;
   messageContent: string;
-  
+
   constructor(public messageService: MessageService) { }
 
-  ngOnInit(): void {}
-
-  sendMessage()
-  {
-    this.messageService.sendMessage(this.username, this.messageContent).then(() =>
-      {
-        this.messageForm.reset();
-      })
+  ngOnInit(): void {
   }
-
+  
+  sendMessage() {
+    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
+      this.messageForm.reset();
+    })
+  }
 }
